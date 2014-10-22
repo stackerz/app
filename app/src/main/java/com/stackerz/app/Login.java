7d1dd6e236c70d20856f3c7b9f48cf7f685f121e
Ed Lima <ed@limagic.com>
2014-10-22 16:00:30 +1100

    commit 4

--- app/src/main/java/com/stackerz/app/Login.java
+++ app/src/main/java/com/stackerz/app/Login.java
@@ -1,18 +1,35 @@
 package com.stackerz.app;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by limedv0 on 17/10/2014.
  */
 public class Login extends Activity {
+
+    Button connect;
+    String username,password;
+    EditText userinput, passinput;
+    SharedPreferences shPref;
+    SharedPreferences.Editor toEdit;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_connect);
-        Bundle extras = getIntent().getExtras();
+        getInit();
+    }
 
+    public void getInit(){
+        connect = (Button)findViewById(R.id.connectButtonLogin);
+        userinput = (EditText)findViewById(R.id.userName);
 
     }
 }
