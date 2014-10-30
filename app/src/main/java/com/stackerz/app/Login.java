3b1ff3cb570ae1f18938c7736c350d7c0ce87dcf
Ed Lima <ed@stackerz.com>
2014-10-30 17:03:21 +1100

    11th Commit

--- app/src/main/java/com/stackerz/app/Login.java
+++ app/src/main/java/com/stackerz/app/Login.java
@@ -198,7 +198,7 @@ public class Login extends Activity implements View.OnClickListener{
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("Accept", "application/json");
-                params.put("Content-Type", "application/json");
+                params.put("Content-Type", "application/json; charset=utf-8");
                 return params;
             }
 
