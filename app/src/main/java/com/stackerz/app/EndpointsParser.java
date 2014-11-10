5ec761e07b11f30fe7d4a740687228409451e1da
Ed Lima <ed@limagic.com>
2014-11-10 14:48:46 +1100

    18th Commit

--- app/src/main/java/com/stackerz/app/EndpointsParser.java
+++ app/src/main/java/com/stackerz/app/EndpointsParser.java
@@ -12,22 +12,24 @@ public class EndpointsParser {
     public static List<Endpoints> parseFeed(String content) {
 
         try {
-            JSONArray ar = new JSONArray(content);
+            Endpoints endpoint = new Endpoints();
+            JSONObject ar = new JSONObject(content);
             List<Endpoints> endpointsList = new ArrayList<>();
+            JSONObject access = ar.getJSONObject("access");
+            JSONArray serviceCatalog = access.getJSONArray("serviceCatalog");
 
-            for (int i = 0; i < ar.length(); i++) {
-
-                JSONObject obj = ar.getJSONObject(i);
-                Endpoints endpoint = new Endpoints();
-
-                endpoint.setName(obj.getString("name"));
-                endpoint.setType(obj.getString("type"));
-                endpoint.setRegion(obj.getString("region"));
-                endpoint.setPublicURL(obj.getString("publicURL"));
-
+            for(int i=0;i<serviceCatalog.length();i++) {
+                JSONObject objsvc = serviceCatalog.getJSONObject(i);
+                JSONArray endpoints = objsvc.getJSONArray("endpoints");
+                endpoint.setName(objsvc.getString("name"));
+                endpoint.setType(objsvc.getString("type"));
+                for (int j = 0; j < objsvc.length(); j++) {
+                    JSONObject objept = endpoints.getJSONObject(j);
+                    endpoint.setRegion(objept.getString("region"));
+                    endpoint.setPublicURL(objept.getString("publicURL"));
+                }
                 endpointsList.add(endpoint);
             }
-
             return endpointsList;
         } catch (JSONException e) {
             e.printStackTrace();
