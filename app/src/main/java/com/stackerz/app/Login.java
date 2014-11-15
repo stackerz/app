4898ad83783a8ce8a6a3885e940541175350e7eb
Ed Lima <ed@stackerz.com>
2014-11-16 00:03:13 +1100

    25th Commit

--- app/src/main/java/com/stackerz/app/Login.java
+++ app/src/main/java/com/stackerz/app/Login.java
@@ -281,7 +281,7 @@ public class Login extends Activity implements View.OnClickListener{
                     public void onErrorResponse(VolleyError error) {
                         VolleyLog.d("App", "Error: " + error.getMessage());
                         Toast toast = Toast.makeText(getApplicationContext(),
-                                "Cannot connect. Check your credentials and try to login again.", Toast.LENGTH_LONG);
+                                "Cannot connect. Check your credentials and try to login again - Data from your last successful session will be available for read-only access", Toast.LENGTH_LONG);
                         toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
                         toast.show();
                         pDialog.hide();
