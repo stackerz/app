d246f02162716c88545cbaf8632f118fc6d45db8
Ed Lima <ed@stackerz.com>
2014-11-17 18:16:00 +1100

    25th Commit

--- app/src/main/java/com/stackerz/app/EndpointsParser.java
+++ app/src/main/java/com/stackerz/app/EndpointsParser.java
@@ -31,7 +31,45 @@ public class EndpointsParser extends Activity{
     public static String keystoneURL = "keystone";
     public static String cinderURL = "cinder";
 
+    public static String getNovaURL() {
+        return novaURL;
+    }
 
+    public static void setNovaURL(String novaURL) {
+        EndpointsParser.novaURL = novaURL;
+    }
+
+    public static String getNeutronURL() {
+        return neutronURL;
+    }
+
+    public static void setNeutronURL(String neutronURL) {
+        EndpointsParser.neutronURL = neutronURL;
+    }
+
+    public static String getGlanceURL() {
+        return glanceURL;
+    }
+
+    public static void setGlanceURL(String glanceURL) {
+        EndpointsParser.glanceURL = glanceURL;
+    }
+
+    public static String getKeystoneURL() {
+        return keystoneURL;
+    }
+
+    public static void setKeystoneURL(String keystoneURL) {
+        EndpointsParser.keystoneURL = keystoneURL;
+    }
+
+    public static String getCinderURL() {
+        return cinderURL;
+    }
+
+    public static void setCinderURL(String cinderURL) {
+        EndpointsParser.cinderURL = cinderURL;
+    }
 
     public static EndpointsParser parser = null;
 
@@ -78,18 +116,23 @@ public class EndpointsParser extends Activity{
             for (Map.Entry<String, String> entry : map.entrySet()) {
                 if (map.containsValue(novaURL)) {
                     novaURL = map.get(PUBLICURL);
+                    setNovaURL(novaURL);
                 }
                 if (map.containsValue(neutronURL)) {
                     neutronURL = map.get(PUBLICURL);
+                    setNeutronURL(neutronURL);
                 }
                 if (map.containsValue(glanceURL)) {
                     glanceURL = map.get(PUBLICURL);
+                    setGlanceURL(glanceURL);
                 }
                 if (map.containsValue(keystoneURL)) {
                     keystoneURL = map.get(PUBLICURL);
+                    setKeystoneURL(keystoneURL);
                 }
                 if (map.containsValue(cinderURL)) {
                     cinderURL = map.get(PUBLICURL);
+                    setCinderURL(cinderURL);
                 }
             }
         }
