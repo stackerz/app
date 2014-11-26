0512ac43c9659cc5fac0f95ba485d57c54a7cb84
Ed Lima <ed@stackerz.com>
2014-11-26 19:09:50 +1100

    30th Commit

--- app/src/main/java/com/stackerz/app/Instances/InstancesFragment.java
+++ app/src/main/java/com/stackerz/app/Instances/InstancesFragment.java
@@ -54,7 +54,8 @@ public class InstancesFragment extends Fragment {
                              Bundle savedInstanceState) {
         Bundle extras = getArguments();
         Serializable parsedList = extras.getSerializable("NovaParsed");
-        if (extras == null){
+
+        if (jsonList == null){
             AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
             alert.setTitle("Token Expired");
             alert.setMessage("Authentication Token expired! Please login again.")
