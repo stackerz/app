c3059644970e614b5fdd9eacd32af7db4942f334
Ed Lima <ed@limagic.com>
2014-12-01 16:09:39 +1100

    Commit 34th

--- app/src/main/java/com/stackerz/app/Instances/NovaAdapter.java
+++ app/src/main/java/com/stackerz/app/Instances/NovaAdapter.java
@@ -77,7 +77,7 @@ class NovaListRowHolder extends RecyclerView.ViewHolder implements View.OnClickL
 
     public void onClick(View view){
         Dialog dialog = new Dialog(view.getContext());
-        dialog.setContentView(R.layout.instances_listdetail);
+        dialog.setContentView(R.layout.instances_list);
         dialog.setTitle("Details " + name.getText() + " " + getPosition());
         dialog.show();
     }
